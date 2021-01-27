de28c40cdb3b58b1c6fe70b50f2d9057fc33f726
alessio <alessiodg99.adg@gmail.com>
2021-01-27 12:09:40 +0100

    fine sezione join_us

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,9 @@ let mix = require('laravel-mix');
 
 mix
 .sass('src/style.scss', 'css')
+    .options({
+        processCssUrls: false
+    })
 .js('src/main.js', 'js')
 .setPublicPath('dist');
+   
